4e7e03c481649274bb7266a5d3c89804b644b2a3
Moussa DIEDHIOU <moussaddh732@gmail.com>
2023-02-01 13:42:34 +0000

    pulling out categorical trees

--- 2-code/Tree_groups.jl
+++ 2-code/Tree_groups.jl
@@ -17,7 +17,17 @@ filter!(:ficelles => x -> x .== "F1", df)
 df[!, :date] = string.(df[!, :date]) # convert type any array to string 
 df_bigening = filter(:date => x -> x .== "2020-11-16", df)
 df_bigening.grp = cut(df_bigening[!, :circonference], 3)
-# XLSX.writetable("grp.xlsx", df_bigening) # ne marche pas a cause de la transformation de la colonne date 
+
+# XLSX.writetable("grp.xlsx", df_bigening) # not working cause of :date column to string transformation in 17 
+
 df_bigening_Q3 = filter(:grp => x -> x .== "Q3: [175.5, 231.5]", df_bigening)
 df_bigening_Q2 = filter(:grp => x -> x .== "Q2: [145.9, 175.5)", df_bigening)
 df_bigening_Q1 = filter(:grp => x -> x .== "Q1: [102.0, 145.9)", df_bigening)
+# avoid the date column 
+select!(df_bigening_Q3, Not([:date]))
+select!(df_bigening_Q2, Not([:date]))
+select!(df_bigening_Q1, Not([:date]))
+
+df_bigening_Q3
+df_bigening_Q2
+df_bigening_Q1
